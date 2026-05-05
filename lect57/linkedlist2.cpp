da8745e73cca85c6fc1805f8bf3ec6b57ba79d86
amandubey923 <kumaraman19137@gmail.com>
2026-05-05 15:42:10 +0530

    feat: implement linked list with push_front and printLL methods

---
+++ lect57/linkedlist2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val); // dynamic object
+        // Node newNode(val); // static object
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void printLL(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp = temp-> next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.printLL();
+}
