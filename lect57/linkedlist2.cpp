8aaad2950fb2f81c2a98b40c728d0669ea104edc
amandubey923 <kumaraman19137@gmail.com>
2026-05-05 23:33:28 +0530

    refactor: remove push_front method from List class and clean up code

--- lect57/linkedlist2.cpp
+++ lect57/linkedlist2.cpp
@@ -23,21 +23,7 @@ public:
     {
         head = tail = NULL;
     }
-    void push_front(int val)
-    {
-        Node *newNode = new Node(val); // dynamic object
-        // Node newNode(val); // static object
-        if (head == NULL)
-        {
-            head = tail = newNode;
-            return;
-        }
-        else
-        {
-            newNode->next = head;
-            head = newNode;
-        }
-    }
+    
     void printLL(){
         Node* temp = head;
         while(temp!=NULL){
---
+++ lect57/ll3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    int data;
+    Node*next;
+
+    Node(int val){
+        data = val ;
+        next= NULL;
+    }
+};
+class List{
+    Node*head;
+    Node* tail;
+    public:
+    List(){
+        head = tail = NULL;
+    }
+    
+};
+
+int main(){
+    List ll;
+
+     
+
+}
