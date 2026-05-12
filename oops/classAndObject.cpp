7292f4fbda1e60b36f1ea13ebe4070bb4af65565
amandubey923 <kumaraman19137@gmail.com>
2026-05-12 18:32:42 +0530

    feat: add configuration files for C++ development and implement Teacher class with methods

---
+++ oops/classAndObject.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher{
+    private:
+    double salary;
+
+    
+// {
+//     public: can be accessed from anywhere
+//     private: can only be accessed within the class
+//     protected: can be accessed within the class and by derived classes 
+// }
+
+    public: //access specifier (public, private, protected)
+
+    //properties or attributes
+    string name;
+    string dept;
+    string subject;
+    
+    //methods or member function (function of class)
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+
+    void setSalary(double s){
+        salary = s;
+    }
+    double getSalary(){
+        return salary;
+    }
+};
+
+
+int main(){
+    Teacher t1;
+    t1.name="Aman";
+    t1.subject="C++";
+    t1.dept="IT";
+    // t1.salary=50000;
+
+    t1.setSalary(50000);
+    cout<<"Name: "<<t1.name<<endl;
+    cout<<"Salary: "<<t1.getSalary()<<endl;
+
+    return 0;
+}
