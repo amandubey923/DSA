beb2582d98614f1ff1c02c2eb5950eef6e86c514
amandubey923 <kumaraman19137@gmail.com>
2026-05-16 12:08:40 +0530

    feat: add Kruskal's algorithm implementation and configuration files for Visual Studio Code

---
+++ KruskalsAlgorithm/KruskalsAlgorithm.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Edge{
+    public:
+    int u,v,wt;
+
+    Edge(int u,int v,int wt){
+        this->u = u;
+        this->v = v;
+        this->wt = wt;
+    }
+    //custom comparator for sorting the edges based on their weights
+    bool operator<(const Edge &other) const {
+        return this->wt < other.wt;
+    }
+};
+class Graph{
+    public:
+    int V;
+    vector<Edge> edges;
+    vector<int> par,rank;
+
+    Graph(int V){
+        this->V = V;
+        for(int i=0;i<V;i++){
+            par.push_back(i);
+            rank.push_back(0);
+        }
+    }
+
+    void addEdge(int u,int v,int wt){
+        edges.push_back(Edge(u,v,wt));
+    }
+    int find(int x){
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]);
+    }
+    void unionByRank(int a,int b){
+        int parA = find(a);
+        int parB = find(b);
+        if(parA == parB) return;
+
+          if(rank[parA] == rank[parB]){
+            par[parA] = parB;
+            rank[parB]++;
+          }else if(rank[parA]>rank[parB]){
+            par[parB] = parA;
+          }else{
+            par[parA]=parB;
+          }
+    }
+
+    void krushkal(){
+        sort(edges.begin(),edges.end());
+        int mstCost = 0;
+        int count = 0;
+        for(int i=0;i<edges.size() && count <V-1;i++){
+            Edge e = edges[i];
+            int parU = find(e.u);
+            int parV = find(e.v);
+
+            if(parU != parV){
+                unionByRank(e.u,e.v);
+                 mstCost += e.wt;
+                 count++;
+            }
+        }
+        cout<<"Minimum Spanning Tree Cost: "<<mstCost<<endl;
+    }
+};
+
+int main(){
+
+    Graph graph(4);
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 2, 6);
+        graph.addEdge(0, 3, 5); 
+        graph.addEdge(1, 3, 15);
+        graph.addEdge(2, 3, 4);
+        graph.krushkal();
+
+    return 0;
+}
