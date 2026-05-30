9a559638edc3fbf262ffb0b2eba7c759d9814e9d
amandubey923 <kumaraman19137@gmail.com>
2026-05-30 20:42:37 +0530

    Stack using stack

--- lect68/stack using stack.cpp
+++ lect68/stack using stack.cpp
@@ -3,19 +3,15 @@
 #include<list>
 #include<stack>
 using namespace std;
-
 int main(){
     stack<int> s;
-
     s.push(10);
     s.push(20);
     s.push(30);
-
     while(!s.empty()){
         cout<< s.top()<< " ";
         s.pop();
     }
     cout<<endl;
-
     return 0;
 }
