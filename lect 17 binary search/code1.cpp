50a4f0fca27cd5e3eeb43785bf8f9021eb16848e
amandubey923 <kumaraman19137@gmail.com>
2026-05-31 20:09:20 +0530

    binary search

--- lect 17 binary search/code1.cpp
+++ lect 17 binary search/code1.cpp
@@ -5,29 +5,22 @@ using namespace std;
 int binarySearch(vector<int> arr, int tar)
 {
     int st = 0, end = arr.size() - 1;
-
-    while (st <= end)
-    {
+    while (st <= end){
         //int mid = (st + end) / 2;
         int mid = st + (end - st)/2;
-        if (tar > arr[mid])
-        {
+        if (tar > arr[mid]){
             st = mid + 1;
         }
-        else if (tar < arr[mid])
-        {
+        else if (tar < arr[mid]){
             end = mid - 1;
         }
-        else
-        {
+        else{
             return mid;
         }
     }
     return -1;
 }
-
-int main()
-{
+int main(){
     vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12}; // odd
     int tar1 = 12;
 
